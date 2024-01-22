aa854f6851f645609364ae546eba426acda8f7c5
Hoster <143075111+ggunjan20@users.noreply.github.com>
2024-01-22 16:08:58 +0530

    restaurant

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
-import DemoPage from "./components/DemoPage/DemoPage"
+import DemoPage from "./components/DemoPage/DemoPage";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Restaurant from './components/Restaurant/Restaurant';
 
 function App() {
   return (
     <div className="App">
-        <DemoPage/>
+      <Router>
+        <Routes>
+          <Route path="/" exact element={<DemoPage />} />
+          <Route path="/Restaurant" exact element={<Restaurant />} />
+          
+        </Routes>
+      </Router>
     </div>
   );
 }
